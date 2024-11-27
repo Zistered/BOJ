f3654983d53c999ac18c7610300c81a4af708d0a
KGB <71389197+Zistered@users.noreply.github.com>
2024-11-27 19:46:05 +0900

    [Bronze III] Title: ACM 호텔, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -1,2 +1,2 @@
 #import<ios>
-main(){int x,y;scanf("%d",&y);while(~scanf("%d%d%d",&x,&y,&y))y--,printf("%d%02d\n",y%x+1,y/x+1);}
+main(){int x,y;for(scanf("%d",&y);~scanf("%d%d%d",&x,&y,&y);)y--,printf("%d%02d ",y%x+1,y/x+1);}
