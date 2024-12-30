64518765e6d8f9e53a4945813dd042eca9e8ed45
KGB <71389197+Zistered@users.noreply.github.com>
2024-12-31 02:22:17 +0900

    [Bronze IV] Title: Juta teekond, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32722. Juta teekond/Juta teekond.py
@@ -0,0 +1,13 @@
+a=input()
+if a!="1" and a!="3":
+    print("EI")
+    exit(0)
+a=input()
+if a!="6" and a!="8":
+    print("EI")
+    exit(0)
+a=input()
+if a!="2" and a!="5":
+    print("EI")
+    exit(0)
+print("JAH")
