05fe2713684a198b693c342f07fbf1de8a79ccc5
KGB <71389197+Zistered@users.noreply.github.com>
2025-04-03 14:11:27 +0900

    [Bronze IV] Title: Yalda, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/33191. Yalda/Yalda.py
@@ -0,0 +1,12 @@
+y=int(input())
+w=int(input())
+p=int(input())
+n=int(input())
+if y>=w:
+    print("Watermelon")
+elif y>=p:
+    print("Pomegranates")
+elif y>=n:
+    print("Nuts")
+else:
+    print("Nothing")
