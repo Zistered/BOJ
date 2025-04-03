ef2ce751b19b3da447812433b514784d89e4aae6
KGB <71389197+Zistered@users.noreply.github.com>
2025-04-03 14:07:09 +0900

    [Bronze IV] Title: Vandalism, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/33651. Vandalism/Vandalism.py
@@ -0,0 +1,18 @@
+u,a,c,p=1,1,1,1
+for i in input():
+    if i=='U':
+        u=0
+    if i=='A':
+        a=0
+    if i=='C':
+        c=0
+    if i=='P':
+        p=0
+if u:
+    print('U', end="")
+if a:
+    print('A', end="")
+if p:
+    print('P', end="")
+if c:
+    print('C', end="")
