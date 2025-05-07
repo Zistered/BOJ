5d8ecb44b0052fa4cca1e497c46ce57fa77358ec
KGB <71389197+Zistered@users.noreply.github.com>
2025-05-07 14:59:14 +0900

    [Bronze III] Title: 부호, Time: 312 ms, Memory: 112168 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.py
@@ -0,0 +1,12 @@
+a=0
+for i in range(int(input())):
+    a+=int(input())
+print(0 if a==0 else '-' if a<0 else '+')
+a=0
+for i in range(int(input())):
+    a+=int(input())
+print(0 if a==0 else '-' if a<0 else '+')
+a=0
+for i in range(int(input())):
+    a+=int(input())
+print(0 if a==0 else '-' if a<0 else '+')
